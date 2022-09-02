8a3e1e1f51c8d8438dd6f1e51e139d6db06e1088
tegrak <luciferanna@gmail.com>
2022-09-02 12:41:47 +0900

    reload.js: remove unused code.

--- reload.js
+++ reload.js
@@ -1,6 +1,5 @@
 const initialize_reload = () => {
 	const isStarted = getTabStorageItem("macro") === "on";
-	getTabStorageItem('macro');
 	if (isStarted) {
 		console.log('reload in ' + (PAGE_TIMEOUT / 1000) + ' seconds');
 		window.setTimeout(
